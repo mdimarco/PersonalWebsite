40b6fc6f3c2ad5deebddfe09628e90095cb708d8
mdimarco <mhd894@gmail.com>
2016-05-27 10:24:31 -0400

    Added personal section subtext

--- javascripts/visibilityToggling.js
+++ javascripts/visibilityToggling.js
@@ -23,3 +23,18 @@ sections.forEach( function (each) {
 	addTogglers(each);
 });
 
+
+//Personal section togglers
+var sections = ['thailand', 'tesla', 'bike'];
+sections.forEach(function(section) {
+	$("#"+section+"-background-img").click( function() {
+		var title = $("#"+section+"-background-img"+" .personal-title");
+		var subText = $("#"+section+"-background-img"+" .small-text");
+		var fadeTime = 400;
+		if (title.css('display') === 'none') {
+			subText.fadeOut(fadeTime, function() { title.fadeIn(); });
+		} else {
+			title.fadeOut(fadeTime, function() { subText.fadeIn(); });
+		}
+	})
+})
